3126625948e1d3caa1d970880cbb1fc514bc13ff
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-09-23 09:55:23 +0800

    September 23 2024 - Laravel Chatify

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -41,6 +41,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
 
+
+
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
